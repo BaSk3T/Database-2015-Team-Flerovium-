21c00e6cbd67e8e2183391e16bc09ad0a1d3afdb
BaSk3T <BaSk3Tt@gmail.com>
2015-10-20 04:13:37 +0300

    Added MSSQLImporter

---
+++ ImportingReports/ImportingReports/MSSQLImport/MSSQLImporter.cs
@@ -0,0 +1,25 @@
+﻿namespace ImportingReports.MSSQLImport
+{
+    using System;
+    using System.Collections;
+    using System.IO;
+
+    using Models;
+    using Newtonsoft.Json;
+
+    public class MSSQLImporter
+    {
+        public static void GetBookInfo(IList sales, string path)
+        {
+            using (StreamReader file = File.OpenText(path))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                Sale sale = (Sale)serializer.Deserialize(file, typeof(Sale));
+
+                sales.Add(sale);
+
+                Console.WriteLine("Read!");
+            }
+        }
+    }
+}
